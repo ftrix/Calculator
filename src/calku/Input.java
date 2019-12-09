0256872616d3a0f2c9dec8cf2223666d408a0eb1
Again <ftrix@yandex.ru>
2019-12-09 22:11:25 +0300

    Calc1.vol2

--- src/calku/Input.java
+++ src/calku/Input.java
@@ -1,13 +1,30 @@
 package calku;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Input{
+public class Input {
     public static int a;
-    public static void chislo (){
-        System.out.println("Введи целое число!");
-        Scanner in = new Scanner(System.in);
-        int input = in.nextInt();
-        a = input;
-        }
+    public static String c;
+    public static boolean b;
+    public static void chislo() {
+            System.out.println("Введи целое число!");
+            Scanner in = new Scanner(System.in);
+            try {
+                int input = in.nextInt();
+                a = input;
+                b = true;
+            }
+            catch (InputMismatchException e) {
+                String input = in.nextLine();
+                c = input;
+                b = false;
+            }
+
+
+
+    }
+
 
 }
+
+
--- src/calku/Main.java
+++ src/calku/Main.java
@@ -4,8 +4,10 @@ import calku.Deystvie;
 public class Main {
 
     public static void main(String[] args){
-        Deystvie chisloOne = new Deystvie();
-        chisloOne.znak();
+
+        Deystvie otvet = new Deystvie();
+        Deystvie.znak();
+
 
 
 
